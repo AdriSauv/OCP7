9d0d53425eafae4fe78d712010a34a85abde890e
Adrien <adriensauvage59@gmail.com>
2023-06-28 22:36:19 +0200

    updated middleware cors/app

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,11 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+const userRoutes = require('./routes/user');
+app.use('/api/auth', userRoutes);
+
+const { cors } = require('./middleware/cors');
+app.use(cors);
 
 mongoose.connect('mongodb+srv://adrien:Crucified62@laegh.plnxqpb.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
