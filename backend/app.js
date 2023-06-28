67dded6e1a85412575c42a4cc69b0b1074429341
Adrien <adriensauvage59@gmail.com>
2023-06-28 21:11:12 +0200

    added cors & schéma Book

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+mongoose.connect('mongodb+srv://adrien:Crucified62@laegh.plnxqpb.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
 
 module.exports = app;
