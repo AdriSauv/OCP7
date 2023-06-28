43c89b15d1d802bb5c3edc1fedfa02268b03e1b9
Adrien <adriensauvage59@gmail.com>
2023-06-28 23:03:43 +0200

    updated signup and sign in && OK

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-app.use(express.json());
-
 const userRoutes = require('./routes/user');
-app.use('/api/auth', userRoutes);
 
-const { cors } = require('./middleware/cors');
-app.use(cors);
+
 
 mongoose.connect('mongodb+srv://adrien:Crucified62@laegh.plnxqpb.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
+    { useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+    
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api/auth/', userRoutes);
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -12,7 +12,7 @@ exports.signup = (req, res, next) => {
             });
             user.save()
                 .then(() => res.status(201).json({ message: 'Utilisateur créé!' }))
-                .catch(error => res.status(400).json({ error }));
+                .catch(error => res.status(400).json({ message:'utilisateur non créé' }));
         })
         .catch(error => res.status(500).json({ error }));
 };
@@ -20,24 +20,26 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            if (user === null) {
+                res.status(401).json({ message: 'Identifiant et/ou mot de passe incorrect' })
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            res.status(401).json({ message: 'Identifiant et/ou mot de passe incorrect' })
+                        } else {
+                            res.status(200).json({
+                                userId: user._id,
+                                token: jwt.sign(
+                                    { userId: user._id },
+                                    'RANDOM_TOKEN_SECRET',
+                                    { expiresIn: '24h' }
+                                )
+                            });
+                        }
+                    })
+                    .catch(error => res.status(500).json({ error }));
             }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
 };
