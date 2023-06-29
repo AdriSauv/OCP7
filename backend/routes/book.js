8a425976572e79cc4e1441f9df688e45391e4868
Adrien <adriensauvage59@gmail.com>
2023-06-29 10:42:19 +0200

    added sharp & optimized webpimages

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const optimizedImg = require('../middleware/sharp-config');
 
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
@@ -10,8 +11,8 @@ router.get('/', bookCtrl.getAllBooks);
 router.get('/bestrating', bookCtrl.getBestBook);
 router.get('/:id', bookCtrl.getOneBook);
 router.post('/:id/rating', auth, bookCtrl.rateBook);
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.updateBook);
+router.post('/', auth, multer, optimizedImg, bookCtrl.createBook);
+router.put('/:id', auth, multer, optimizedImg, bookCtrl.updateBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
