60ce8f971d0a72284b232e2056ecc55eaa773766
Adrien <adriensauvage59@gmail.com>
2023-06-29 10:22:30 +0200

    added bestBooks & rateBook

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -7,9 +7,11 @@ const multer = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
+router.get('/bestrating', bookCtrl.getBestBook);
 router.get('/:id', bookCtrl.getOneBook);
-router.put('/:id', auth, multer, bookCtrl.updateBook);
+router.post('/:id/rating', auth, bookCtrl.rateBook);
 router.post('/', auth, multer, bookCtrl.createBook);
+router.put('/:id', auth, multer, bookCtrl.updateBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
