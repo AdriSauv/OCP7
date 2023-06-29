a964455aa738a4b6609e9bceed8520e176451a18
Adrien <adriensauvage59@gmail.com>
2023-06-29 16:21:22 +0200

    code cleanup

--- backend/middleware/password-validator.js
+++ backend/middleware/password-validator.js
@@ -2,7 +2,7 @@ const passwordValidator = require('password-validator');
 
 const passwordSchema = new passwordValidator();
 passwordSchema
-    .is().min(8)
+    .is().min(6)
     .is().max(100)
     .has().uppercase()
     .has().lowercase()
